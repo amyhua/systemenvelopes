049cd44354745a08ee2b706fda8fa6632dae86b6
Amy Hua <foramyhua@gmail.com>
2022-01-25 22:37:34 -0800

    cleanup

--- components/EnvelopeItem.tsx
+++ components/EnvelopeItem.tsx
@@ -33,7 +33,7 @@ const EnvelopeItem = ({
   period: EnvelopePeriod,
   periodType: ActivePeriodType,
 }) => {
-  const startLog = useState(false);
+  const startLog: State = useState(false);
   const periodPronoun = periodType === ActivePeriodType.This ? 'this' : 'last';
   const spentHealthTextColorClassName = spentActivePeriod !== undefined &&
     targetSpend < spentActivePeriod ?
@@ -47,10 +47,10 @@ const EnvelopeItem = ({
   }
   return (
     <div className={`bg-white py-5 border-b border-gray-200 sm:px-2`}>
-      <div className="
-        cursor-pointer
+      <div className={`}
+        ${startLog.get() ? 'cursor-auto' : 'cursor-pointer'}
         -ml-4 flex justify-between items-center
-        flex-wrap sm:flex-nowrap"
+        flex-wrap sm:flex-nowrap`}
       onClick={() => startLog.set(true)}>
         <div className="ml-4">
           <h3 className="text-lg leading-6 font-medium text-gray-900">
@@ -146,11 +146,11 @@ const LogSpendForm = ({ onSubmit, period, onCancelForm }: {
   period: EnvelopePeriod,
   onCancelForm: () => void
 }) => {
-  const amountState = useState(0)
-  const amountOnFocus = useState(false)
-  const amountChangeBlur = useState(false)
-  const cancelling = useState(false)
-  const activePeriodType = useState(ActivePeriodType.This)
+  const amountState: State = useState(0)
+  const amountOnFocus: State = useState(false)
+  const amountChangeBlur: State = useState(false)
+  const cancelling: State = useState(false)
+  const activePeriodType: State = useState(ActivePeriodType.This)
   const amountDeltaOnClickFn = (delta: number) => () => {
     amountState.set(amountState.get() + delta)
     amountChangeBlur.set(true)
@@ -166,181 +166,187 @@ const LogSpendForm = ({ onSubmit, period, onCancelForm }: {
     setTimeout(() => amountChangeBlur.set(false), 75);
   }
   return (
-    <div className={`${styles.logSpendForm} spend-form ml-2 mt-5 mb-5 h-auto ${
+    <div className={`${styles.logSpendForm} spend-form ml-3 mb-5 h-auto ${
       cancelling.get() ? styles.cancelling : ''
     }`} style={{
       overflow: 'hidden',
       transition: 'height 0.3s ease-in-out, margin-top 0.3s ease-in-out, margin-bottom 0.3s ease-in-out'
     }}>
-      <div className="mb-1 mt-3">
-        <span className="inline-block align-middle mb-2">
-          <button
-            type="button"
-            className="ml-3 relative inline-flex items-center
-              align-top mt-1
-              font-medium
-              text-gray-700 hover:text-green-500
-              "
-          >
-            <MinusCircleIcon
-              onClick={() => {
-                amountState.set(amountState.get() - 1)
-                amountChangeBlur.set(true)
-              }}
-              className="h-10 w-10" /> 
-          </button>
-          {
-            amountOnFocus.get() ?
-            <input
-              style={{width: 150}}
-              className="text-4xl mt-1 text-gray-700 text-center inline-block"
-              type="number"
-              value={amountState.get() === 0 ? '' : amountState.get()}
-              autoFocus={true}
-              onChange={e => amountState.set(Number(e.target.value))}
-              onFocus={() => amountOnFocus.set(true)}
-              onBlur={() => amountOnFocus.set(false)}
-            /> :
-            <span
-              style={{width: 150}}
-              onClick={() => amountOnFocus.set(true)}
-              className={`text-center inline-block text-gray-700 ${
-                amountState.get() > 999 ? 'mt-2 text-2xl' : 'mt-1 text-4xl'
-              }`}>
-                <span className={`
-                  ${amountChangeBlur.get() ? (
-                    amountState.get() < 0 ? 'text-green-500' : 'text-red-500'
-                  ) : 
-                    'text-gray-700'
-                  } 
-                  ease-in-out
-                `}>
-                  ${
-                    Number(amountState.get().toFixed(2)).toLocaleString(undefined, {minimumFractionDigits: 2})
-                  }
-                </span>
-            </span>
-          }
-          <button
-            type="button"
-            className="relative inline-flex items-center
-              mr-1 align-top mt-1
-              font-medium
-              text-gray-700 hover:text-red-500
-              "
-          >
-            <PlusCircleIcon
-              onClick={() => {
-                amountState.set(amountState.get() + 1)
-                amountChangeBlur.set(true)
-              }}
-              className="h-10 w-10" /> 
-          </button>
-        </span>
-        <button type="submit"
-          style={{marginTop: 2, width: 150}}
-          onClick={() => {
-            onSubmit(amountState.get(), activePeriodType.get())
-            onCancelHandler()
-          }}
-          className={classNames(
-            'inline-block',
-            'w-full py-2.5 mr-2 ml-5 mr-4 text-sm leading-5 font-medium text-black rounded-lg',
-            'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-gray-800 ring-white ring-opacity-60',
-            'bg-gray-800 hover:bg-gray-700 ease-in text-white px-3'
-          )}>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        onSubmit(amountState.get(), activePeriodType.get())
+        onCancelHandler()
+      }}>
+        <div className="mb-1 mt-3">
+          <span className="inline-block align-middle mb-2">
+            <button
+              type="button"
+              className="ml-3 relative inline-flex items-center
+                align-top mt-1
+                font-medium
+                text-gray-700 hover:text-green-500
+                "
+            >
+              <MinusCircleIcon
+                onClick={() => {
+                  amountState.set(amountState.get() - 1)
+                  amountChangeBlur.set(true)
+                }}
+                className="h-10 w-10" /> 
+            </button>
             {
-              amountState.get() < 0 ?
-              'Log Addition' : 'Log Spend'
+              amountOnFocus.get() ?
+              <input
+                style={{width: 150}}
+                className="text-4xl mt-1 text-gray-700 text-center inline-block"
+                type="number"
+                value={amountState.get() === 0 ? '' : amountState.get()}
+                autoFocus={true}
+                onChange={e => amountState.set(Number(e.target.value))}
+                onFocus={() => amountOnFocus.set(true)}
+                onBlur={() => amountOnFocus.set(false)}
+              /> :
+              <span
+                style={{width: 150}}
+                onClick={() => amountOnFocus.set(true)}
+                className={`text-center inline-block text-gray-700 ${
+                  amountState.get() > 999 ? 'mt-2 text-2xl' : 'mt-1 text-4xl'
+                }`}>
+                  <span className={`
+                    ${amountChangeBlur.get() ? (
+                      amountState.get() < 0 ? 'text-green-500' : 'text-red-500'
+                    ) : 
+                      'text-gray-700'
+                    } 
+                    ease-in-out
+                  `}>
+                    ${
+                      Number(amountState.get().toFixed(2)).toLocaleString(undefined, {minimumFractionDigits: 2})
+                    }
+                  </span>
+              </span>
             }
-        </button>
-        <span className="inline-block md:ml-0 ml-6" style={{width: 225}}>
-          <Tab.Group onChange={(index: number) => {
-            const periodType = Object.keys(periodLoggedLabel(period))[index]
-            activePeriodType.set(periodType as ActivePeriodType)
-          }}>
-            <Tab.List className="flex p-1 bg-gray-800 rounded-xl">
-              {Object.keys(periodLoggedLabel(period)).map((periodType: string) => (
-                <Tab
-                  key={periodType}
-                  className={({ selected }) =>
-                    classNames(
-                      'w-full py-2 text-sm leading-5 font-medium text-black rounded-lg',
-                      'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-gray-700 ring-white ring-opacity-60',
-                      selected
-                        ? 'bg-white shadow'
-                        : 'text-white hover:bg-white/[0.12] hover:text-white'
-                    )
-                  }
-              >
-                {periodLoggedLabel(period)[periodType as ActivePeriodType]}
-              </Tab>
-            ))}
-          </Tab.List>
-          </Tab.Group>
-        </span>
-      </div>
-      <div className="flex-shrink-0">
-        <div className="mt-4" style={{marginLeft: 16}}>
-          <span className="relative z-0 bg-gray-700 text-white inline-flex shadow-sm rounded-md">
             <button
               type="button"
-              onClick={amountDeltaOnClickFn(5)}
-              className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50
-                focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
+              className="relative inline-flex items-center
+                mr-1 align-top mt-1
+                font-medium
+                text-gray-700 hover:text-red-500
+                "
             >
-              +$5
-            </button>
-            <button
-              type="button"
-              onClick={amountDeltaOnClickFn(10)}
-              className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
-            >
-              +$10
-            </button>
-            <button
-              type="button"
-              onClick={amountDeltaOnClickFn(50)}
-              className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
-            >
-              +$50
-            </button>
-            <button
-              type="button"
-              onClick={amountDeltaOnClickFn(100)}
-              className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
-            >
-              +$100
-            </button>
-            <button
-              type="button"
-              onClick={amountDeltaOnClickFn(500)}
-              className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
-            >
-              +$500
-            </button>
-            <button
-              type="button"
-              onClick={amountDeltaOnClickFn(1000)}
-              className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
-            >
-              +$1K
-            </button>
-            <button
-              type="button"
-              onClick={amountDeltaOnClickFn(5000)}
-              className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
-            >
-              +$5K
+              <PlusCircleIcon
+                onClick={() => {
+                  amountState.set(amountState.get() + 1)
+                  amountChangeBlur.set(true)
+                }}
+                className="h-10 w-10" /> 
             </button>
           </span>
+          <button type="submit"
+            style={{marginTop: 2, width: 150}}
+            onClick={() => {
+              onSubmit(amountState.get(), activePeriodType.get())
+              onCancelHandler()
+            }}
+            className={classNames(
+              'inline-block',
+              'w-full py-2.5 mr-2 ml-5 mr-4 text-sm leading-5 font-medium text-black rounded-lg',
+              'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-gray-800 ring-white ring-opacity-60',
+              'bg-gray-800 hover:bg-gray-700 ease-in text-white px-3'
+            )}>
+              {
+                amountState.get() < 0 ?
+                'Log Addition' : 'Log Spend'
+              }
+          </button>
+          <span className="inline-block md:ml-0 ml-6" style={{width: 225}}>
+            <Tab.Group onChange={(index: number) => {
+              const periodType = Object.keys(periodLoggedLabel(period))[index]
+              activePeriodType.set(periodType as ActivePeriodType)
+            }}>
+              <Tab.List className="flex p-1 bg-gray-800 rounded-xl">
+                {Object.keys(periodLoggedLabel(period)).map((periodType: string) => (
+                  <Tab
+                    key={periodType}
+                    className={({ selected }) =>
+                      classNames(
+                        'w-full py-2 text-sm leading-5 font-medium text-black rounded-lg',
+                        'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-gray-700 ring-white ring-opacity-60',
+                        selected
+                          ? 'bg-white shadow'
+                          : 'text-white hover:bg-white/[0.12] hover:text-white'
+                      )
+                    }
+                >
+                  {periodLoggedLabel(period)[periodType as ActivePeriodType]}
+                </Tab>
+              ))}
+            </Tab.List>
+            </Tab.Group>
+          </span>
         </div>
-        <div className="mt-4 ml-5 text-sm">
-          <a href="#"
-          onClick={onCancelHandler}
-          className="underline text-gray-500 hover:text-gray-800">Cancel</a>
+        <div className="flex-shrink-0">
+          <div className="mt-4" style={{marginLeft: 16}}>
+            <span className="relative z-0 bg-gray-700 text-white inline-flex shadow-sm rounded-md">
+              <button
+                type="button"
+                onClick={amountDeltaOnClickFn(5)}
+                className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50
+                  focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
+              >
+                +$5
+              </button>
+              <button
+                type="button"
+                onClick={amountDeltaOnClickFn(10)}
+                className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
+              >
+                +$10
+              </button>
+              <button
+                type="button"
+                onClick={amountDeltaOnClickFn(50)}
+                className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
+              >
+                +$50
+              </button>
+              <button
+                type="button"
+                onClick={amountDeltaOnClickFn(100)}
+                className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
+              >
+                +$100
+              </button>
+              <button
+                type="button"
+                onClick={amountDeltaOnClickFn(500)}
+                className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
+              >
+                +$500
+              </button>
+              <button
+                type="button"
+                onClick={amountDeltaOnClickFn(1000)}
+                className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
+              >
+                +$1K
+              </button>
+              <button
+                type="button"
+                onClick={amountDeltaOnClickFn(amountState.get() * -1)}
+                className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
+              >
+                Reset
+              </button>
+            </span>
+          </div>
+          <div className="mt-4 ml-5 text-sm">
+            <a href="#"
+            onClick={onCancelHandler}
+            className="underline text-gray-500 hover:text-gray-800">Cancel</a>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
